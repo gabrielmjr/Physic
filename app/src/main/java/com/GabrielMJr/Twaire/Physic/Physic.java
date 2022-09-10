d7f8ba9c9122c6cfffdf85f2fca62d1fa125af6d
gabrielmjr <gmjplayys@gmail.com>
2022-09-10 20:57:32 +0200

    1.0.1

--- app/src/main/java/com/GabrielMJr/Twaire/Physic/Physic.java
+++ app/src/main/java/com/GabrielMJr/Twaire/Physic/Physic.java
@@ -1,9 +1,8 @@
-package com.GabrielMJr.Twaire.Physic;
+package com.gabrielMJr.twaire.physic;
 
-import com.GabrielMJr.Twaire.Physic.PhysicManager;
-import com.GabrielMJr.Twaire.tools.NumberAnalyst;
+import com.gabrielMJr.twaire.tools.Tools;
 
-public class Physic implements PhysicManager{
+public class Physic{
   
   public static void main(String[] args) {
     return;
@@ -11,56 +10,8 @@ public class Physic implements PhysicManager{
   
   // Atalho para normalizar expoente
   protected String expNormalizer(Double value) {
-    NumberAnalyst NumberAnalyst = new NumberAnalyst();
-    String[] list = NumberAnalyst.expNormalizer(value);
-    return NumberAnalyst.normalize(list);
+    Tools tools = new Tools();
+    Long[] list = tools.expNormalizer(value);
+    return tools.normalize(list);
   }
-  
-  // Imicio de Vazão 
-  @Override
-  public String ffluidFlow(Double volume, Double time) {
-    return null;
-  } 
-  
-  @Override
-  public String sfluidFlow(Double area, Double velocidade) {
-    return null;
-  }
-  
-  @Override
-  public String tfluidFlow(Double raio, Double velocidade) {
-    return null;
-  }
-  
-  @Override
-  public String tempo(Double volume, Double fluidflow) {
-    return null;
-  }
-  
-    @Override
-  public String volume(Double tempo, Double fluidflow) {
-    return null;
-  }
-  
-  @Override
-  public String fvelocidade(Double fluidflow, Double area) {
-    return null;
-  }
-  
-  @Override
-  public String svelocidade(Double fluidflow, Double raio) {
-    return null;
-  }
-
-  @Override
-  public String area(Double fluidflow, Double velocidade) {
-    return null;
-  }
-  
-  @Override
-  public String raio(Double fluidflow, Double velocidade) {
-    return null;
-  }
-  
-  // Fim de Vazão 
 }
---
+++ app/src/main/java/com/GabrielMJr/Twaire/Physic/fluidFlow/FluidFlowIF.java
@@ -0,0 +1,13 @@
+package com.gabrielMJr.twaire.physic;
+
+public interface FluidFlowIF {
+    public abstract String fFluidFlow(double volume, double time);
+    public abstract String sFluidFlow(double area, double velocidade);
+    public abstract String tFluidFlow(double raio, double velocidade);
+    public abstract String time(double volume, double fluidFlow);
+    public abstract String volume(double tempo, double fluidFlow);
+    public abstract String fSpeed(double fFluidFlow, double area);
+    public abstract String sSpeed(double fluidFlow, double raio);
+    public abstract String area(double fluidFlow, double velocidade);
+    public abstract String ray(double fluidFlow, double velocidade);
+}
