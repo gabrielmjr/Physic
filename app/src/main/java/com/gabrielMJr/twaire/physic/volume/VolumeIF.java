0b6b84d08435faa91baf05ffec24b959a31a47ef
gabrielmjr <gmjplayys@gmail.com>
2022-10-14 18:29:14 +0000

    1.0.4-beta_2

--- app/src/main/java/com/gabrielMJr/twaire/physic/volume/VolumeIF.java
+++
@@ -1,7 +0,0 @@
-package com.gabrielMJr.twaire.physic.volume;
-
-public interface VolumeIF {
-  public abstract double to(double value, String value_unit);
-  
-  //public abstract double (double )
-}
