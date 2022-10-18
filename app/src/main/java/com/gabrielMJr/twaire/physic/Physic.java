6748d3bcfc6189adc93ec72c88b1dd715f050ba3
gabrielmjr <gmjplayys@gmail.com>
2022-10-18 19:32:37 +0200

    1.0.4-Beta_2

--- app/src/main/java/com/gabrielMJr/twaire/physic/Physic.java
+++ app/src/main/java/com/gabrielMJr/twaire/physic/Physic.java
@@ -4,7 +4,7 @@ import com.gabrielMJr.twaire.tools.Tools;
 
 public class Physic{
 
-  private static final String version = "1.0.3";
+  private static final String version = "1.0.4-Beta_2";
   private static final String creator = "Gabriel MJr";
   
   // Those constants were used to tell to the program if you want only results or steps
--- app/src/main/java/com/gabrielMJr/twaire/physic/Volume.java
+++ app/src/main/java/com/gabrielMJr/twaire/physic/Volume.java
@@ -1,14 +1,68 @@
-package com.gabrielMJr.twaire.physic;
+package com.gabrielMjr.twaire.physic;
 
-public class Volume {
+import com.gabrielMJr.twaire.physic.VolumeIF;
 
-   // Those constants were used to tell to the program the unit that must use to calculate something
+
+public class Volume implements VolumeIF{
+  
+    // Those constants were used to tell to the program the unit that must use to calculate something
   // Also used to tell the unit from the variables as parameter
+  
   public final String CUBE_KILOMETER = "0.000000001";
   public final String CUBE_HECTOMETER = "0.000001";
   public final String CUBE_DECAMETER = "0.001";
   public final String CUBE_METER = "1";
   public final String CUBE_DECIMETER = "1000";
+  public final String CUBE_CENTIMETER = "1000000";
+  public final String CUBE_MILIMETER = "1000000000";
   
-  
+  // 
+  @Override
+  public double toCubeKilometer (
+    double value,
+    String value_unit) {
+      return ((value * Double.valueOf (CUBE_KILOMETER)) / Double.valueOf (value_unit));
+    }
+      
+      @Override
+  public double toCubeHectometer (
+    double value,
+    String value_unit) {
+      return ((value * Double.valueOf (CUBE_HECTOMETER)) / Double.valueOf (value_unit));
+    }
+    
+      @Override
+  public double toCubeDecameter (
+    double value,
+    String value_unit) {
+      return ((value * Double.valueOf (CUBE_DECAMETER)) / Double.valueOf (value_unit));
+    }
+    
+      @Override
+  public double toCubeMeter (
+    double value,
+    String value_unit) {
+      return ((value * Double.valueOf (CUBE_METER)) / Double.valueOf (value_unit));
+    }
+    
+      @Override
+  public double toCubeDecimeter (
+    double value,
+    String value_unit) {
+      return ((value * Double.valueOf (CUBE_DECIMETER)) / Double.valueOf (value_unit));
+    }
+    
+      @Override
+  public double toCubeCentimeter (
+    double value,
+    String value_unit) {
+      return ((value * Double.valueOf (CUBE_CENTIMETER)) / Double.valueOf (value_unit));
+    }
+    
+      @Override
+  public double toCubeMilimeter (
+    double value,
+    String value_unit) {
+      return ((value * Double.valueOf (CUBE_MILIMETER)) / Double.valueOf (value_unit));
+    }
 }
