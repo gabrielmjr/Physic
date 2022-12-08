a8760138f234f8a20186d448abdd141395d8bf80
Gabriel M Júnior <gmjplayys@gmail.com>
2022-12-08 13:37:14 +0200

    Added junit testing for physic>>muv

---
+++ src/test/java/com/gabrielMJr/twaire/muv/Displacement2Test.java
@@ -0,0 +1,41 @@
+package com.gabrielMJr.twaire.physic.muv;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static com.gabrielMJr.twaire.physic.Physic.GET_RESULT;
+
+/*
+ This class tests the mru >> Displacement2.class
+*/
+public class Displacement2Test
+{
+    // MRU object
+    private MUV muv;
+    
+    // Constructor
+    public Displacement2Test()
+    {
+        // Initialize the variable
+        muv = new MUV();
+    }
+    
+    
+    /*
+     This method tests Displacement2 class
+    */
+    @Test
+    public void displacement2Test()
+    {
+        // The value to be tested:
+        Double initialSpeed = 6.0;
+        Double deltaTime = 2.0;
+        Double acceleration = 3.0;
+        Double displacement = 21.0;
+        
+        // The result need to ne:
+        String result = "3.0";
+        
+        // Testing
+        assertEquals(result, muv.displacement2(initialSpeed, deltaTime, acceleration, displacement, GET_RESULT));
+    }
+}
