07f276b8ad1b9c0ad4f38764ae231c04de8d533f
Gabriel M Júnior <gmjplayys@gmail.com>
2023-01-14 17:49:14 +0200

    Added junit testing to distance conversor >> from anothers units to decameter

--- src/test/java/com/gabrielMJr/units/DistanceTest.java
+++ src/test/java/com/gabrielMJr/units/DistanceTest.java
@@ -90,4 +90,42 @@ public class DistanceTest
     
     assertEquals(millimeter_result, distance.toHectometer(millimeter_value, Distance.MILLIMETER));
   }
+  
+  
+  // Test converter from another units to  decameter
+  @Test 
+  public void toDecameterTest()
+  {
+    // Values to be tested 
+    double kilometer_value = 374.0;
+    double hectometer_value = 826.0;
+    double decameter_value = 628.0;
+    double meter_value = 726.0;
+    double decimeter_value = 688.0;
+    double centimeter_value = 786.0;
+    double millimeter_value = 518.0;
+    
+    // Results
+    double kilometer_result = 37400.0;
+    double hectometer_result = 8260.0;
+    double decameter_result = 628.0;
+    double meter_result = 72.60000000000001;
+    double decimeter_result = 6.88;
+    double centimeter_result = 0.786;
+    double millimeter_result = 0.051800000000000006;
+    
+     assertEquals(kilometer_result, distance.toDecameter(kilometer_value, distance.KILOMETER));
+    
+    assertEquals(hectometer_result, distance.toDecameter(hectometer_value, distance.HECTOMETER));
+    
+    assertEquals(decameter_result, distance.toDecameter(decameter_value, distance.DECAMETER));
+    
+    assertEquals(meter_result, distance.toDecameter(meter_value, distance.METER));
+    
+    assertEquals(decimeter_result, distance.toDecameter(decimeter_value, distance.DECIMETER));
+    
+    assertEquals(centimeter_result, distance.toDecameter(centimeter_value, distance.CENTIMETER));
+    
+    assertEquals(millimeter_result, distance.toDecameter(millimeter_value, Distance.MILLIMETER));
+  }
 }
