356c2abd581d6698d11d699047eb94a56565fa9b
Gabriel M Júnior <gmjplayys@gmail.com>
2023-01-15 10:24:27 +0200

    Fixed build CI #37

--- src/test/java/com/gabrielMJr/kinematics/mru/Displacement1Test.java
+++ src/test/java/com/gabrielMJr/kinematics/mru/Displacement1Test.java
@@ -1,4 +1,4 @@
-package com.gabrielMJr.physic.kinematics.kinematics.mru;
+package com.gabrielMJr.physic.kinematics.mru;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
