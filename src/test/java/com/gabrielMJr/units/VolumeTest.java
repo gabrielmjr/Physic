8d7d6e69318ee4b30327fe31f1ae2350a25f1aff
Gabriel M Júnior <gmjplayys@gmail.com>
2023-01-17 08:19:00 +0200

    Added junit testing to volume conversor >> from anothers units to hectometer

--- src/test/java/com/gabrielMJr/units/VolumeTest.java
+++ src/test/java/com/gabrielMJr/units/VolumeTest.java
@@ -16,7 +16,7 @@ public class VolumeTest
   }
   
   
-  // Test conversor from another units to second
+  // Test conversor from another units to kilometer 
   @Test
   public void toCubeKilometerTest()
   {
@@ -53,5 +53,44 @@ public class VolumeTest
     
     assertEquals(cube_millimeter_result, volume.toCubeKilometer(cube_millimeter_value, Volume.CUBE_MILLIMETER));
     
+  }
+  
+  
+  // Test conversor from another units to hectometer 
+  @Test
+  public void toCubeHectometerTest()
+  {
+    // Values to be testes
+    double cube_kilometer_value = 1.0;
+    double cube_hectometer_value = 1.0;
+    double cube_decameter_value = 1.0;
+    double cube_meter_value = 1.0;
+    double cube_decimeter_value = 1.0;
+    double cube_centimeter_value = 1.0;
+    double cube_millimeter_value = 1.0;
+    
+    // Result Values
+    Double cube_kilometer_result = 999.9999999999999;
+    Double cube_hectometer_result = 1.0;
+    Double cube_decameter_result = 0.001;
+    Double cube_meter_result = 0.000001;
+    Double cube_decimeter_result = 0.0000000009999999999999999;
+    Double cube_centimeter_result = 0.000000000001;
+    Double cube_millimeter_result = 0.0000000000000009999999999999999;
+    
+    // Testing
+    assertEquals(cube_kilometer_result, volume.toCubeHectometer(cube_kilometer_value, Volume.CUBE_KILOMETER));
+    
+    assertEquals(cube_hectometer_result, volume.toCubeHectometer(cube_hectometer_value, Volume.CUBE_HECTOMETER));
+    
+    assertEquals(cube_decameter_result, volume.toCubeHectometer(cube_decameter_value, Volume.CUBE_DECAMETER));
+    
+    assertEquals(cube_meter_result, volume.toCubeHectometer(cube_meter_value, Volume.CUBE_METER));
+    
+    assertEquals(cube_decimeter_result, volume.toCubeHectometer(cube_decimeter_value, Volume.CUBE_DECIMETER));
+    
+    assertEquals(cube_centimeter_result, volume.toCubeHectometer(cube_centimeter_value, Volume.CUBE_CENTIMETER));
+    
+    assertEquals(cube_millimeter_result, volume.toCubeHectometer(cube_millimeter_value, Volume.CUBE_MILLIMETER));
   }
 }
