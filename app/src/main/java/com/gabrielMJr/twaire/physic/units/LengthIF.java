030c094dbe776cd8dc73bbb6ba45e4e101a6f249
Gabriel M Júnior <gmjplayys@gmail.com>
2023-02-26 15:47:38 +0200

    Added padronized data type on unit converter

--- app/src/main/java/com/gabrielMJr/twaire/physic/units/LengthIF.java
+++ app/src/main/java/com/gabrielMJr/twaire/physic/units/LengthIF.java
@@ -3,15 +3,15 @@ package com.gabrielMJr.twaire.physic.units;
 public interface LengthIF {
   public abstract double toKilometer(double value, String unit);
 
-  public abstract double toHectometer(double value, String unit);
+  public abstract Double toHectometer(double value, String unit);
   
-  public abstract double toDecameter(double value, String unit);
+  public abstract Double toDecameter(double value, String unit);
   
-  public abstract double toMeter(double value, String unit);
+  public abstract Double toMeter(double value, String unit);
   
-  public abstract double toDecimeter(double value, String unit);
+  public abstract Double toDecimeter(double value, String unit);
   
-  public abstract double toCentimeter(double value, String unit);
+  public abstract Double toCentimeter(double value, String unit);
   
-  public abstract double toMillimeter(double value, String unit);
+  public abstract Double toMillimeter(double value, String unit);
 }
