5a414118b9182f272d43fa9a9ffc09e0b15ca48b
Gabriel M Júnior <gmjplayys@gmail.com>
2023-03-02 20:55:23 +0200

    Changed internal values in speed converter

--- app/src/main/java/com/gabrielMJr/twaire/physic/units/SpeedIF.java
+++ app/src/main/java/com/gabrielMJr/twaire/physic/units/SpeedIF.java
@@ -4,27 +4,27 @@ import java.math.BigDecimal;
 
 public interface SpeedIF 
 {
-	public abstract BigDecimal toKilometerPerSecond(double value, String unit);
-	public abstract BigDecimal toHectometerPerSecond(double value, String unit);
-	public abstract BigDecimal toDecameterPerSecond(double value, String unit);
-	public abstract BigDecimal toMeterPerSecond(double value, String unit);
-	public abstract BigDecimal toDecimeterPerSecond(double value, String unit);
-	public abstract BigDecimal toCentimeterPerSecond(double value, String unit);
-	public abstract BigDecimal toMillimeterPerSecond(double value, String unit);
+	public abstract BigDecimal toKilometerPerSecond(double value, int unit);
+	public abstract BigDecimal toHectometerPerSecond(double value, int unit);
+	public abstract BigDecimal toDecameterPerSecond(double value, int unit);
+	public abstract BigDecimal toMeterPerSecond(double value, int unit);
+	public abstract BigDecimal toDecimeterPerSecond(double value, int unit);
+	public abstract BigDecimal toCentimeterPerSecond(double value, int unit);
+	public abstract BigDecimal toMillimeterPerSecond(double value, int unit);
 
-	public abstract BigDecimal toKilometerPerMinute(double value, String unit);
-	public abstract BigDecimal toHectometerPerMinute(double value, String unit);
-	public abstract BigDecimal toDecameterPerMinute(double value, String unit);
-	public abstract BigDecimal toMeterPerMinute(double value, String unit);
-	public abstract BigDecimal toDecimeterPerMinute(double value, String unit);
-	public abstract BigDecimal toCentimeterPerMinute(double value, String unit);
-	public abstract BigDecimal toMillimeterPerMinute(double value, String unit);
+	public abstract BigDecimal toKilometerPerMinute(double value, int unit);
+	public abstract BigDecimal toHectometerPerMinute(double value, int unit);
+	public abstract BigDecimal toDecameterPerMinute(double value, int unit);
+	public abstract BigDecimal toMeterPerMinute(double value, int unit);
+	public abstract BigDecimal toDecimeterPerMinute(double value, int unit);
+	public abstract BigDecimal toCentimeterPerMinute(double value, int unit);
+	public abstract BigDecimal toMillimeterPerMinute(double value, int unit);
 	
-	public abstract BigDecimal toKilometerPerHour(double value, String unit);
-	public abstract BigDecimal toHectometerPerHour(double value, String unit);
-	public abstract BigDecimal toDecameterPerHour(double value, String unit);
-	public abstract BigDecimal toMeterPerHour(double value, String unit);
-	public abstract BigDecimal toDecimeterPerHour(double value, String unit);
-	public abstract BigDecimal toCentimeterPerHour(double value, String unit);
-	public abstract BigDecimal toMillimeterPerHour(double value, String unit);
+	public abstract BigDecimal toKilometerPerHour(double value, int unit);
+	public abstract BigDecimal toHectometerPerHour(double value, int unit);
+	public abstract BigDecimal toDecameterPerHour(double value, int unit);
+	public abstract BigDecimal toMeterPerHour(double value, int unit);
+	public abstract BigDecimal toDecimeterPerHour(double value, int unit);
+	public abstract BigDecimal toCentimeterPerHour(double value, int unit);
+	public abstract BigDecimal toMillimeterPerHour(double value, int unit);
 }
