5ff486d9f1054b2f2649b8405d43a542c6fda2de
Gabriel M Júnior <gmjplayys@gmail.com>
2023-03-04 10:58:20 +0200

    formatted kinematics >> muv

--- app/src/main/java/com/gabrielMJr/twaire/physic/kinematics/muv/Speed2.java
+++ app/src/main/java/com/gabrielMJr/twaire/physic/kinematics/muv/Speed2.java
@@ -4,73 +4,72 @@ import com.gabrielMJr.twaire.physic.Physic;
 
 final class Speed2
 {
-  // This object variable
-  private static Speed2 instance; 
-  
-   // Private constructor to avoid new classe's incence
-  private Speed2(){}
-  
-  protected String speed (
-      double initialTime,
-      double finalTime, 
-      double acceleration,
-      int stepOrResult)
-      {
+	// This object variable
+	private static Speed2 instance; 
+
+	// Private constructor to avoid new classe's incence
+	private Speed2 ()
+	{}
+
+	protected String speed (
+		double initialTime,
+		double finalTime, 
+		double acceleration,
+		int stepOrResult)
+	{
         double step1 = finalTime - initialTime;
         double step2 = step1 * acceleration;
         String res;
-        
-        if (stepOrResult == Physic.GET_STEP) {
-          res = "∆v = ("
-              + finalTime
-              + "s - "
-              + initialTime
-              + "s) × ("
-              + acceleration
-              + "m/s²)"
-              + "\n∆v = "
-              + step1
-              + "s × ("
-              + acceleration
-              + "m/s²)"
-              + "\n∆v = "
-              + step2
-              + "m/s";
-              
-              
-          return res;
-          
-        } else if (stepOrResult == Physic.GET_RESULT) {
-          return String.valueOf(step2);
-          
-        } else {
-          return "Error 404: fourth parameter not found.";
+        if (stepOrResult == Physic.GET_STEP)
+		{
+			res = "∆v = ("
+				+ finalTime
+				+ "s - "
+				+ initialTime
+				+ "s) × ("
+				+ acceleration
+				+ "m/s²)"
+				+ "\n∆v = "
+				+ step1
+				+ "s × ("
+				+ acceleration
+				+ "m/s²)"
+				+ "\n∆v = "
+				+ step2
+				+ "m/s";
+			return res;
         }
-      }
-      
-  protected String speed (
-      double initialTime,
-      String initialTimeUnit,
-      double finalTime, 
-      String finalTimeUnit,
-      double acceleration,
-      String accelerationUnit,
-      String unitOfResult,
-      int stepOrResult)    
-  {
-        
+		else if (stepOrResult == Physic.GET_RESULT)
+		{
+			return String.valueOf(step2);
+        }
+		else
+		{
+			return "Error 404: fourth parameter not found.";
+        }
+	}
+
+	protected String speed (
+		double initialTime,
+		String initialTimeUnit,
+		double finalTime, 
+		String finalTimeUnit,
+		double acceleration,
+		String accelerationUnit,
+		String unitOfResult,
+		int stepOrResult)    
+	{
         return null;
-  }
-  
-  
-  // Get class instance object
-    public static Speed2 getInstance()
+	}
+
+
+	// Get class instance object
+    public static Speed2 getInstance ()
     {
-      if(instance == null)
-      {
-        instance = new Speed2();
-      }
-      
-      return instance;
+		if (instance == null)
+		{
+			instance = new Speed2();
+		}
+		return instance;
     }
 }
--- app/src/main/java/com/gabrielMJr/twaire/physic/kinematics/muv/Time3.java
+++ app/src/main/java/com/gabrielMJr/twaire/physic/kinematics/muv/Time3.java
@@ -4,72 +4,73 @@ import com.gabrielMJr.twaire.physic.Physic;
 
 final class Time3
 {
-  // This object variable
-  private static Time3 instance; 
-  
-   // Private constructor to avoid new classe's incence
-  private Time3(){}
-  
-  protected String time (
-          double finalTime,
-          double deltaSpeed,
-          double acceleration,
-          int stepOrResult)
-          {
-            double step1 = deltaSpeed / acceleration;
-            double step2 = finalTime - step1;
-            String res;
-            
-            if (stepOrResult == Physic.GET_STEP) {
-              res = "ti = "
-                  + finalTime
-                  + "s - "
-                  + deltaSpeed
-                  + "m/s ÷ "
-                  + acceleration
-                  + "m/s²"
-                  + "\nti = "
-                  + finalTime
-                  + "s - "
-                  + step1
-                  + "s"
-                  + "\nti = "
-                  + step2
-                  + "s";
-                  
-              return res;
-               
-            } else if (stepOrResult == Physic.GET_RESULT) {
-              return String.valueOf(step2);
-              
-            } else {
-              return "Error 404: fourth parameter not found.";
-            }
-          }
+	// This object variable
+	private static Time3 instance; 
+
+	// Private constructor to avoid new classe's incence
+	private Time3 ()
+	{}
+
+	protected String time (
+		double finalTime,
+		double deltaSpeed,
+		double acceleration,
+		int stepOrResult)
+	{
+		double step1 = deltaSpeed / acceleration;
+		double step2 = finalTime - step1;
+		String res;
+		if (stepOrResult == Physic.GET_STEP)
+		{
+			res = "ti = "
+				+ finalTime
+				+ "s - "
+				+ deltaSpeed
+				+ "m/s ÷ "
+				+ acceleration
+				+ "m/s²"
+				+ "\nti = "
+				+ finalTime
+				+ "s - "
+				+ step1
+				+ "s"
+				+ "\nti = "
+				+ step2
+				+ "s";
+			return res;
+		}
+		else if (stepOrResult == Physic.GET_RESULT)
+		{
+			return String.valueOf(step2);
+		}
+		else
+		{
+			return "Error 404: fourth parameter not found.";
+		}
+	}
 
     public String time (
-    double finalTime,
-    String finalTimeUnit,
-    double deltaSpeed, 
-    String deltaSpeedUnit,
-    double acceleration, 
-    String accelerationUnit,
-    String unitOfResult,
-    int stepOrResult)
-    
+		double finalTime,
+		String finalTimeUnit,
+		double deltaSpeed, 
+		String deltaSpeedUnit,
+		double acceleration, 
+		String accelerationUnit,
+		String unitOfResult,
+		int stepOrResult)
+
     {
-      return null;
+		return null;
     }
-    
-    
+
+
     // Get class instance object
-    public static Time3 getInstance()
+    public static Time3 getInstance ()
     {
-      if(instance == null)
-      {
-        instance = new Time3();
-      }
-      
-      return instance;
+		if (instance == null)
+		{
+			instance = new Time3();
+		}
+		return instance;
     }
 }
