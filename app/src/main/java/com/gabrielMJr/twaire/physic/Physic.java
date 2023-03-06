45f7ec4bdef938792366b9ff14af531d03bfbe5e
Gabriel M Júnior <gmjplayys@gmail.com>
2023-03-06 08:55:06 +0200

    Formatted physic class

--- app/src/main/java/com/gabrielMJr/twaire/physic/Physic.java
+++ app/src/main/java/com/gabrielMJr/twaire/physic/Physic.java
@@ -3,11 +3,12 @@ package com.gabrielMJr.twaire.physic;
 public class Physic
 {
 
-  // Those constants were used to tell to the program if you want only results or steps
-  public static final int GET_RESULT = 0;
-  public static final int GET_STEP = 1;
-  
-  public static void main(String[] args) {
-	return;
-  }
+	// Those constants were used to tell to the program if you want only results or steps
+	public static final int GET_RESULT = 0;
+	public static final int GET_STEP = 1;
+
+	public static void main (String[] args)
+	{
+		return;
+	}
 }
