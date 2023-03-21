0ad3e68d83cd6cfa2624f7bc7e2d1b13c4e457a2
Gabriel M Júnior <gmjplayys@gmail.com>
2023-03-21 12:23:24 +0200

    Prepared vertical launch >> down ward class structure

--- app/src/main/java/com/gabrielMJr/twaire/physic/kinematics/vertical_launch/downward/DownWard.java
+++ app/src/main/java/com/gabrielMJr/twaire/physic/kinematics/vertical_launch/downward/DownWard.java
@@ -2,55 +2,7 @@ package com.gabrielMJr.twaire.physic.kinematics.vertical_launch.downward;
 
 public class DownWard implements DownWardIF {
     private static DownWard instance;
+	
+    public DownWard () {}
 
-    private Height height;
-
-
-    public DownWard () {
-        height = Height.getInstance();
-    }
-
-    @Override
-    public String height (
-        double initialHeight,
-        double initialVelocity,
-        double time,
-        double gravity, 
-        int resultOrStep) {
-		return height.calculateHeight(
-			initialHeight,
-			initialVelocity,
-			time,
-			gravity,
-			resultOrStep);
-	}
-
-    @Override
-    public String height (
-        double initialHeight,
-		String initialHeightUnit,
-        double initialVelocity, 
-		String initialVelocityUnit,
-        double time,
-		String timeUnit,
-        double gravity,
-		String gravityUnit,
-        String unitOfResult,
-        int resultOrStep) {
-		return height.calculateHeight(initialHeight,
-									  initialHeightUnit,
-									  initialVelocity,
-									  initialVelocityUnit,
-									  time, timeUnit,
-									  gravity,
-									  gravityUnit,
-									  unitOfResult,
-									  resultOrStep);
-	}
-
-    protected static DownWard getInstance () {
-        if (!(instance instanceof DownWard))
-            instance = new DownWard();
-        return instance;
-    }
 }
