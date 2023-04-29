142d8df48f59fddf43a3a08014e0126d61e4beb5
Gariel MJr <gmjplayys@gmail.com>
2023-04-29 07:01:49 +0200

    Added toSquareSecond converter test

---
+++ app/src/test/java/com/mjr/code/physic/units/SquareTimeTest.java
@@ -0,0 +1,63 @@
+package com.mjr.code.physic.units;
+
+import com.mjr.code.physic.PhysicException;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.Random;
+import org.junit.jupiter.api.Test;
+
+import static com.mjr.code.physic.Physic.ROUND_SCALE;
+import static com.mjr.code.physic.units.Time.toSquareSecond;
+import static com.mjr.code.physic.units.Time.toSquareMinute;
+import static com.mjr.code.physic.units.Time.toSquareHour;
+import static com.mjr.code.physic.units.Time.toSquareDay;
+import static com.mjr.code.physic.units.Time.toSquareWeek;
+import static com.mjr.code.physic.units.Time.SQUARE_TIME_SCALES;
+import static com.mjr.code.physic.units.Time.SQUARE_SECOND;
+import static com.mjr.code.physic.units.Time.SQUARE_MINUTE;
+import static com.mjr.code.physic.units.Time.SQUARE_HOUR;
+import static com.mjr.code.physic.units.Time.SQUARE_DAY;
+import static com.mjr.code.physic.units.Time.SQUARE_WEEK;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SquareTimeTest {
+	private Random random;
+	
+	public SquareTimeTest() {
+		random = new Random();
+	}
+	
+	@Test
+	public void toSquareSecondTest() throws PhysicException {
+		double squareSecondValue = getRandomDouble();
+		double squareMinuteValue = getRandomDouble();
+		double squareHourValue = getRandomDouble();
+		double squareDayValue = getRandomDouble();
+		double squareWeekValue = getRandomDouble();
+		try {
+			Time squareSecondTestResult = new Time(BigDecimal.valueOf(squareSecondValue).multiply(SQUARE_TIME_SCALES[SQUARE_SECOND - 26]).divide(SQUARE_TIME_SCALES[SQUARE_SECOND - 26], ROUND_SCALE, RoundingMode.HALF_UP), SQUARE_SECOND);
+			Time squareMinuteTestResult = new Time(BigDecimal.valueOf(squareMinuteValue).multiply(SQUARE_TIME_SCALES[SQUARE_MINUTE - 26]).divide(SQUARE_TIME_SCALES[SQUARE_SECOND - 26], ROUND_SCALE, RoundingMode.HALF_UP), SQUARE_MINUTE);
+			Time squareHourTestResult = new Time(BigDecimal.valueOf(squareHourValue).multiply(SQUARE_TIME_SCALES[SQUARE_HOUR - 26]).divide(SQUARE_TIME_SCALES[SQUARE_SECOND - 26], ROUND_SCALE, RoundingMode.HALF_UP), SQUARE_HOUR);
+			Time squareDayTestResult = new Time(BigDecimal.valueOf(squareDayValue).multiply(SQUARE_TIME_SCALES[SQUARE_DAY - 26]).divide(SQUARE_TIME_SCALES[SQUARE_SECOND - 26], ROUND_SCALE, RoundingMode.HALF_UP), SQUARE_DAY);
+			Time squareWeekTestResult = new Time(BigDecimal.valueOf(squareWeekValue).multiply(SQUARE_TIME_SCALES[SQUARE_WEEK - 26]).divide(SQUARE_TIME_SCALES[SQUARE_SECOND - 26], ROUND_SCALE, RoundingMode.HALF_UP), SQUARE_WEEK);
+			
+			Time squareSecondResult = toSquareSecond(squareSecondValue, SQUARE_SECOND);
+			Time squareMinuteResult = toSquareSecond(squareMinuteValue, SQUARE_MINUTE);
+			Time squareHourResult = toSquareSecond(squareHourValue, SQUARE_HOUR);
+			Time squareDayResult = toSquareSecond(squareDayValue, SQUARE_DAY);
+			Time squareWeekResult = toSquareSecond(squareWeekValue, SQUARE_WEEK);
+			
+			assertEquals(squareSecondTestResult, squareSecondResult);
+			assertEquals(squareMinuteTestResult, squareMinuteResult);
+			assertEquals(squareHourTestResult, squareHourResult);
+			assertEquals(squareDayTestResult, squareDayResult);
+			assertEquals(squareWeekTestResult, squareWeekResult);
+		} catch (PhysicException e) {
+			throw e;
+		}
+	}
+	
+	private double getRandomDouble() {
+		return random.nextDouble();
+	}
+}
