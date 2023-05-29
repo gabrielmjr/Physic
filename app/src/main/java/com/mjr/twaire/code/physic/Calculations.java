527b78f1b4e9e76ac9bbf1af75b67debbbf19ca2
GabrielMJr <gmjplayys@gmail.com>
2023-05-29 08:30:20 +0200

    Created calculations interface

---
+++ app/src/main/java/com/mjr/twaire/code/physic/Calculations.java
@@ -0,0 +1,7 @@
+package com.mjr.twaire.code.physic;
+
+public interface Calculations {
+	public abstract double getResult();
+	public abstract String getSteps();
+	public abstract String getFormula();
+}
