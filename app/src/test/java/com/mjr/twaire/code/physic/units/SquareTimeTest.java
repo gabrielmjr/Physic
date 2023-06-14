58dba28271b78ae6c45dcdaf634b9c7bba3e4738
GabrielMJr <gmjplayys@gmail.com>
2023-06-14 13:25:49 +0200

    fix test: Fixed square time target measuring unit in test

--- app/src/test/java/com/mjr/twaire/code/physic/units/SquareTimeTest.java
+++ app/src/test/java/com/mjr/twaire/code/physic/units/SquareTimeTest.java
@@ -30,10 +30,10 @@ public class SquareTimeTest {
 		double squareWeekValue = getRandomDouble();
 		try {
 			Time squareSecondTestResult = new Time(BigDecimal.valueOf(squareSecondValue).multiply(SQUARE_TIME_SCALES[SQUARE_SECOND - 26]).divide(SQUARE_TIME_SCALES[SQUARE_SECOND - 26], ROUND_SCALE, RoundingMode.HALF_UP), SQUARE_SECOND);
-			Time squareMinuteTestResult = new Time(BigDecimal.valueOf(squareMinuteValue).multiply(SQUARE_TIME_SCALES[SQUARE_SECOND - 26]).divide(SQUARE_TIME_SCALES[SQUARE_MINUTE - 26], ROUND_SCALE, RoundingMode.HALF_UP), SQUARE_MINUTE);
-			Time squareHourTestResult = new Time(BigDecimal.valueOf(squareHourValue).multiply(SQUARE_TIME_SCALES[SQUARE_SECOND - 26]).divide(SQUARE_TIME_SCALES[SQUARE_HOUR - 26], ROUND_SCALE, RoundingMode.HALF_UP), SQUARE_HOUR);
-			Time squareDayTestResult = new Time(BigDecimal.valueOf(squareDayValue).multiply(SQUARE_TIME_SCALES[SQUARE_SECOND - 26]).divide(SQUARE_TIME_SCALES[SQUARE_DAY - 26], ROUND_SCALE, RoundingMode.HALF_UP), SQUARE_DAY);
-			Time squareWeekTestResult = new Time(BigDecimal.valueOf(squareWeekValue).multiply(SQUARE_TIME_SCALES[SQUARE_SECOND - 26]).divide(SQUARE_TIME_SCALES[SQUARE_WEEK - 26], ROUND_SCALE, RoundingMode.HALF_UP), SQUARE_WEEK);
+			Time squareMinuteTestResult = new Time(BigDecimal.valueOf(squareMinuteValue).multiply(SQUARE_TIME_SCALES[SQUARE_SECOND - 26]).divide(SQUARE_TIME_SCALES[SQUARE_MINUTE - 26], ROUND_SCALE, RoundingMode.HALF_UP), SQUARE_SECOND);
+			Time squareHourTestResult = new Time(BigDecimal.valueOf(squareHourValue).multiply(SQUARE_TIME_SCALES[SQUARE_SECOND - 26]).divide(SQUARE_TIME_SCALES[SQUARE_HOUR - 26], ROUND_SCALE, RoundingMode.HALF_UP), SQUARE_SECOND);
+			Time squareDayTestResult = new Time(BigDecimal.valueOf(squareDayValue).multiply(SQUARE_TIME_SCALES[SQUARE_SECOND - 26]).divide(SQUARE_TIME_SCALES[SQUARE_DAY - 26], ROUND_SCALE, RoundingMode.HALF_UP), SQUARE_SECOND);
+			Time squareWeekTestResult = new Time(BigDecimal.valueOf(squareWeekValue).multiply(SQUARE_TIME_SCALES[SQUARE_SECOND - 26]).divide(SQUARE_TIME_SCALES[SQUARE_WEEK - 26], ROUND_SCALE, RoundingMode.HALF_UP), SQUARE_SECOND);
 			
 			Time squareSecondResult = toSquareSecond(squareSecondValue, SQUARE_SECOND);
 			Time squareMinuteResult = toSquareSecond(squareMinuteValue, SQUARE_MINUTE);
@@ -59,11 +59,11 @@ public class SquareTimeTest {
 		double squareDayValue = getRandomDouble();
 		double squareWeekValue = getRandomDouble();
 		try {
-			Time squareSecondTestResult = new Time(BigDecimal.valueOf(squareSecondValue).multiply(SQUARE_TIME_SCALES[SQUARE_MINUTE - 26]).divide(SQUARE_TIME_SCALES[SQUARE_SECOND - 26], ROUND_SCALE, RoundingMode.HALF_UP), SQUARE_SECOND);
+			Time squareSecondTestResult = new Time(BigDecimal.valueOf(squareSecondValue).multiply(SQUARE_TIME_SCALES[SQUARE_MINUTE - 26]).divide(SQUARE_TIME_SCALES[SQUARE_SECOND - 26], ROUND_SCALE, RoundingMode.HALF_UP), SQUARE_MINUTE);
 			Time squareMinuteTestResult = new Time(BigDecimal.valueOf(squareMinuteValue).multiply(SQUARE_TIME_SCALES[SQUARE_MINUTE - 26]).divide(SQUARE_TIME_SCALES[SQUARE_MINUTE - 26], ROUND_SCALE, RoundingMode.HALF_UP), SQUARE_MINUTE);
-			Time squareHourTestResult = new Time(BigDecimal.valueOf(squareHourValue).multiply(SQUARE_TIME_SCALES[SQUARE_MINUTE - 26]).divide(SQUARE_TIME_SCALES[SQUARE_HOUR - 26], ROUND_SCALE, RoundingMode.HALF_UP), SQUARE_HOUR);
-			Time squareDayTestResult = new Time(BigDecimal.valueOf(squareDayValue).multiply(SQUARE_TIME_SCALES[SQUARE_MINUTE - 26]).divide(SQUARE_TIME_SCALES[SQUARE_DAY - 26], ROUND_SCALE, RoundingMode.HALF_UP), SQUARE_DAY);
-			Time squareWeekTestResult = new Time(BigDecimal.valueOf(squareWeekValue).multiply(SQUARE_TIME_SCALES[SQUARE_MINUTE - 26]).divide(SQUARE_TIME_SCALES[SQUARE_WEEK - 26], ROUND_SCALE, RoundingMode.HALF_UP), SQUARE_WEEK);
+			Time squareHourTestResult = new Time(BigDecimal.valueOf(squareHourValue).multiply(SQUARE_TIME_SCALES[SQUARE_MINUTE - 26]).divide(SQUARE_TIME_SCALES[SQUARE_HOUR - 26], ROUND_SCALE, RoundingMode.HALF_UP), SQUARE_MINUTE);
+			Time squareDayTestResult = new Time(BigDecimal.valueOf(squareDayValue).multiply(SQUARE_TIME_SCALES[SQUARE_MINUTE - 26]).divide(SQUARE_TIME_SCALES[SQUARE_DAY - 26], ROUND_SCALE, RoundingMode.HALF_UP), SQUARE_MINUTE);
+			Time squareWeekTestResult = new Time(BigDecimal.valueOf(squareWeekValue).multiply(SQUARE_TIME_SCALES[SQUARE_MINUTE - 26]).divide(SQUARE_TIME_SCALES[SQUARE_WEEK - 26], ROUND_SCALE, RoundingMode.HALF_UP), SQUARE_MINUTE);
 
 			Time squareSecondResult = toSquareMinute(squareSecondValue, SQUARE_SECOND);
 			Time squareMinuteResult = toSquareMinute(squareMinuteValue, SQUARE_MINUTE);
@@ -89,11 +89,11 @@ public class SquareTimeTest {
 		double squareDayValue = getRandomDouble();
 		double squareWeekValue = getRandomDouble();
 		try {
-			Time squareSecondTestResult = new Time(BigDecimal.valueOf(squareSecondValue).multiply(SQUARE_TIME_SCALES[SQUARE_HOUR - 26]).divide(SQUARE_TIME_SCALES[SQUARE_SECOND - 26], ROUND_SCALE, RoundingMode.HALF_UP), SQUARE_SECOND);
-			Time squareMinuteTestResult = new Time(BigDecimal.valueOf(squareMinuteValue).multiply(SQUARE_TIME_SCALES[SQUARE_HOUR - 26]).divide(SQUARE_TIME_SCALES[SQUARE_MINUTE - 26], ROUND_SCALE, RoundingMode.HALF_UP), SQUARE_MINUTE);
+			Time squareSecondTestResult = new Time(BigDecimal.valueOf(squareSecondValue).multiply(SQUARE_TIME_SCALES[SQUARE_HOUR - 26]).divide(SQUARE_TIME_SCALES[SQUARE_SECOND - 26], ROUND_SCALE, RoundingMode.HALF_UP), SQUARE_HOUR);
+			Time squareMinuteTestResult = new Time(BigDecimal.valueOf(squareMinuteValue).multiply(SQUARE_TIME_SCALES[SQUARE_HOUR - 26]).divide(SQUARE_TIME_SCALES[SQUARE_MINUTE - 26], ROUND_SCALE, RoundingMode.HALF_UP), SQUARE_HOUR);
 			Time squareHourTestResult = new Time(BigDecimal.valueOf(squareHourValue).multiply(SQUARE_TIME_SCALES[SQUARE_HOUR - 26]).divide(SQUARE_TIME_SCALES[SQUARE_HOUR - 26], ROUND_SCALE, RoundingMode.HALF_UP), SQUARE_HOUR);
-			Time squareDayTestResult = new Time(BigDecimal.valueOf(squareDayValue).multiply(SQUARE_TIME_SCALES[SQUARE_HOUR - 26]).divide(SQUARE_TIME_SCALES[SQUARE_DAY - 26], ROUND_SCALE, RoundingMode.HALF_UP), SQUARE_DAY);
-			Time squareWeekTestResult = new Time(BigDecimal.valueOf(squareWeekValue).multiply(SQUARE_TIME_SCALES[SQUARE_HOUR - 26]).divide(SQUARE_TIME_SCALES[SQUARE_WEEK - 26], ROUND_SCALE, RoundingMode.HALF_UP), SQUARE_WEEK);
+			Time squareDayTestResult = new Time(BigDecimal.valueOf(squareDayValue).multiply(SQUARE_TIME_SCALES[SQUARE_HOUR - 26]).divide(SQUARE_TIME_SCALES[SQUARE_DAY - 26], ROUND_SCALE, RoundingMode.HALF_UP), SQUARE_HOUR);
+			Time squareWeekTestResult = new Time(BigDecimal.valueOf(squareWeekValue).multiply(SQUARE_TIME_SCALES[SQUARE_HOUR - 26]).divide(SQUARE_TIME_SCALES[SQUARE_WEEK - 26], ROUND_SCALE, RoundingMode.HALF_UP), SQUARE_HOUR);
 
 			Time squareSecondResult = toSquareHour(squareSecondValue, SQUARE_SECOND);
 			Time squareMinuteResult = toSquareHour(squareMinuteValue, SQUARE_MINUTE);
@@ -119,11 +119,11 @@ public class SquareTimeTest {
 		double squareDayValue = getRandomDouble();
 		double squareWeekValue = getRandomDouble();
 		try {
-			Time squareSecondTestResult = new Time(BigDecimal.valueOf(squareSecondValue).multiply(SQUARE_TIME_SCALES[SQUARE_DAY - 26]).divide(SQUARE_TIME_SCALES[SQUARE_SECOND - 26], ROUND_SCALE, RoundingMode.HALF_UP), SQUARE_SECOND);
-			Time squareMinuteTestResult = new Time(BigDecimal.valueOf(squareMinuteValue).multiply(SQUARE_TIME_SCALES[SQUARE_DAY - 26]).divide(SQUARE_TIME_SCALES[SQUARE_MINUTE - 26], ROUND_SCALE, RoundingMode.HALF_UP), SQUARE_MINUTE);
-			Time squareHourTestResult = new Time(BigDecimal.valueOf(squareHourValue).multiply(SQUARE_TIME_SCALES[SQUARE_DAY - 26]).divide(SQUARE_TIME_SCALES[SQUARE_HOUR - 26], ROUND_SCALE, RoundingMode.HALF_UP), SQUARE_HOUR);
+			Time squareSecondTestResult = new Time(BigDecimal.valueOf(squareSecondValue).multiply(SQUARE_TIME_SCALES[SQUARE_DAY - 26]).divide(SQUARE_TIME_SCALES[SQUARE_SECOND - 26], ROUND_SCALE, RoundingMode.HALF_UP), SQUARE_DAY);
+			Time squareMinuteTestResult = new Time(BigDecimal.valueOf(squareMinuteValue).multiply(SQUARE_TIME_SCALES[SQUARE_DAY - 26]).divide(SQUARE_TIME_SCALES[SQUARE_MINUTE - 26], ROUND_SCALE, RoundingMode.HALF_UP), SQUARE_DAY);
+			Time squareHourTestResult = new Time(BigDecimal.valueOf(squareHourValue).multiply(SQUARE_TIME_SCALES[SQUARE_DAY - 26]).divide(SQUARE_TIME_SCALES[SQUARE_HOUR - 26], ROUND_SCALE, RoundingMode.HALF_UP), SQUARE_DAY);
 			Time squareDayTestResult = new Time(BigDecimal.valueOf(squareDayValue).multiply(SQUARE_TIME_SCALES[SQUARE_DAY - 26]).divide(SQUARE_TIME_SCALES[SQUARE_DAY - 26], ROUND_SCALE, RoundingMode.HALF_UP), SQUARE_DAY);
-			Time squareWeekTestResult = new Time(BigDecimal.valueOf(squareWeekValue).multiply(SQUARE_TIME_SCALES[SQUARE_DAY - 26]).divide(SQUARE_TIME_SCALES[SQUARE_WEEK - 26], ROUND_SCALE, RoundingMode.HALF_UP), SQUARE_WEEK);
+			Time squareWeekTestResult = new Time(BigDecimal.valueOf(squareWeekValue).multiply(SQUARE_TIME_SCALES[SQUARE_DAY - 26]).divide(SQUARE_TIME_SCALES[SQUARE_WEEK - 26], ROUND_SCALE, RoundingMode.HALF_UP), SQUARE_DAY);
 
 			Time squareSecondResult = toSquareDay(squareSecondValue, SQUARE_SECOND);
 			Time squareMinuteResult = toSquareDay(squareMinuteValue, SQUARE_MINUTE);
@@ -149,10 +149,10 @@ public class SquareTimeTest {
 		double squareDayValue = getRandomDouble();
 		double squareWeekValue = getRandomDouble();
 		try {
-			Time squareSecondTestResult = new Time(BigDecimal.valueOf(squareSecondValue).multiply(SQUARE_TIME_SCALES[SQUARE_WEEK - 26]).divide(SQUARE_TIME_SCALES[SQUARE_SECOND - 26], ROUND_SCALE, RoundingMode.HALF_UP), SQUARE_SECOND);
-			Time squareMinuteTestResult = new Time(BigDecimal.valueOf(squareMinuteValue).multiply(SQUARE_TIME_SCALES[SQUARE_WEEK - 26]).divide(SQUARE_TIME_SCALES[SQUARE_MINUTE - 26], ROUND_SCALE, RoundingMode.HALF_UP), SQUARE_MINUTE);
-			Time squareHourTestResult = new Time(BigDecimal.valueOf(squareHourValue).multiply(SQUARE_TIME_SCALES[SQUARE_WEEK - 26]).divide(SQUARE_TIME_SCALES[SQUARE_HOUR - 26], ROUND_SCALE, RoundingMode.HALF_UP), SQUARE_HOUR);
-			Time squareDayTestResult = new Time(BigDecimal.valueOf(squareDayValue).multiply(SQUARE_TIME_SCALES[SQUARE_WEEK - 26]).divide(SQUARE_TIME_SCALES[SQUARE_DAY - 26], ROUND_SCALE, RoundingMode.HALF_UP), SQUARE_DAY);
+			Time squareSecondTestResult = new Time(BigDecimal.valueOf(squareSecondValue).multiply(SQUARE_TIME_SCALES[SQUARE_WEEK - 26]).divide(SQUARE_TIME_SCALES[SQUARE_SECOND - 26], ROUND_SCALE, RoundingMode.HALF_UP), SQUARE_WEEK);
+			Time squareMinuteTestResult = new Time(BigDecimal.valueOf(squareMinuteValue).multiply(SQUARE_TIME_SCALES[SQUARE_WEEK - 26]).divide(SQUARE_TIME_SCALES[SQUARE_MINUTE - 26], ROUND_SCALE, RoundingMode.HALF_UP), SQUARE_WEEK);
+			Time squareHourTestResult = new Time(BigDecimal.valueOf(squareHourValue).multiply(SQUARE_TIME_SCALES[SQUARE_WEEK - 26]).divide(SQUARE_TIME_SCALES[SQUARE_HOUR - 26], ROUND_SCALE, RoundingMode.HALF_UP), SQUARE_WEEK);
+			Time squareDayTestResult = new Time(BigDecimal.valueOf(squareDayValue).multiply(SQUARE_TIME_SCALES[SQUARE_WEEK - 26]).divide(SQUARE_TIME_SCALES[SQUARE_DAY - 26], ROUND_SCALE, RoundingMode.HALF_UP), SQUARE_WEEK);
 			Time squareWeekTestResult = new Time(BigDecimal.valueOf(squareWeekValue).multiply(SQUARE_TIME_SCALES[SQUARE_WEEK - 26]).divide(SQUARE_TIME_SCALES[SQUARE_WEEK - 26], ROUND_SCALE, RoundingMode.HALF_UP), SQUARE_WEEK);
 
 			Time squareSecondResult = toSquareWeek(squareSecondValue, SQUARE_SECOND);
@@ -181,10 +181,10 @@ public class SquareTimeTest {
 		int squareWeekValue = 1;
 		try {
 			Time squareSecondTestResult = new Time("1.00000000000000000000", SQUARE_SECOND);
-			Time squareMinuteTestResult = new Time("3600.00000000000000000000", SQUARE_MINUTE);
-			Time squareHourTestResult = new Time("12960000.00000000000000000000", SQUARE_HOUR);
-			Time squareDayTestResult = new Time("7464960000.00000000000000000000", SQUARE_DAY);
-			Time squareWeekTestResult = new Time("365783040000.00000000000000000000", SQUARE_WEEK);
+			Time squareMinuteTestResult = new Time("3600.00000000000000000000", SQUARE_SECOND);
+			Time squareHourTestResult = new Time("12960000.00000000000000000000", SQUARE_SECOND);
+			Time squareDayTestResult = new Time("7464960000.00000000000000000000", SQUARE_SECOND);
+			Time squareWeekTestResult = new Time("365783040000.00000000000000000000", SQUARE_SECOND);
 
 			Time squareSecondResult = toSquareSecond(squareSecondValue, SQUARE_SECOND);
 			Time squareMinuteResult = toSquareSecond(squareMinuteValue, SQUARE_MINUTE);
@@ -210,11 +210,11 @@ public class SquareTimeTest {
 		int squareDayValue = 1;
 		int squareWeekValue = 1;
 		try {
-			Time squareSecondTestResult = new Time("0.00027777777777777778", SQUARE_SECOND);
+			Time squareSecondTestResult = new Time("0.00027777777777777778", SQUARE_MINUTE);
 			Time squareMinuteTestResult = new Time("1.00000000000000000000", SQUARE_MINUTE);
-			Time squareHourTestResult = new Time("3600.00000000000000000000", SQUARE_HOUR);
-			Time squareDayTestResult = new Time("2073600.00000000000000000000", SQUARE_DAY);
-			Time squareWeekTestResult = new Time("101606400.00000000000000000000", SQUARE_WEEK);
+			Time squareHourTestResult = new Time("3600.00000000000000000000", SQUARE_MINUTE);
+			Time squareDayTestResult = new Time("2073600.00000000000000000000", SQUARE_MINUTE);
+			Time squareWeekTestResult = new Time("101606400.00000000000000000000", SQUARE_MINUTE);
 
 			Time squareSecondResult = toSquareMinute(squareSecondValue, SQUARE_SECOND);
 			Time squareMinuteResult = toSquareMinute(squareMinuteValue, SQUARE_MINUTE);
@@ -240,11 +240,11 @@ public class SquareTimeTest {
 		int squareDayValue = 1;
 		int squareWeekValue = 1;
 		try {
-			Time squareSecondTestResult = new Time("0.00000007716049382716", SQUARE_SECOND);
-			Time squareMinuteTestResult = new Time("0.00027777777777777778", SQUARE_MINUTE);
+			Time squareSecondTestResult = new Time("0.00000007716049382716", SQUARE_HOUR);
+			Time squareMinuteTestResult = new Time("0.00027777777777777778", SQUARE_HOUR);
 			Time squareHourTestResult = new Time("1.00000000000000000000", SQUARE_HOUR);
-			Time squareDayTestResult = new Time("576.00000000000000000000", SQUARE_DAY);
-			Time squareWeekTestResult = new Time("28224.00000000000000000000", SQUARE_WEEK);
+			Time squareDayTestResult = new Time("576.00000000000000000000", SQUARE_HOUR);
+			Time squareWeekTestResult = new Time("28224.00000000000000000000", SQUARE_HOUR);
 
 			Time squareSecondResult = toSquareHour(squareSecondValue, SQUARE_SECOND);
 			Time squareMinuteResult = toSquareHour(squareMinuteValue, SQUARE_MINUTE);
@@ -270,11 +270,11 @@ public class SquareTimeTest {
 		int squareDayValue = 1;
 		int squareWeekValue = 1;
 		try {
-			Time squareSecondTestResult = new Time("0.00000000013395919067", SQUARE_SECOND);
-			Time squareMinuteTestResult = new Time("0.00000048225308641975", SQUARE_MINUTE);
-			Time squareHourTestResult = new Time("0.00173611111111111111", SQUARE_HOUR);
+			Time squareSecondTestResult = new Time("0.00000000013395919067", SQUARE_DAY);
+			Time squareMinuteTestResult = new Time("0.00000048225308641975", SQUARE_DAY);
+			Time squareHourTestResult = new Time("0.00173611111111111111", SQUARE_DAY);
 			Time squareDayTestResult = new Time("1.00000000000000000000", SQUARE_DAY);
-			Time squareWeekTestResult = new Time("49.00000000000000000000", SQUARE_WEEK);
+			Time squareWeekTestResult = new Time("49.00000000000000000000", SQUARE_DAY);
 
 			Time squareSecondResult = toSquareDay(squareSecondValue, SQUARE_SECOND);
 			Time squareMinuteResult = toSquareDay(squareMinuteValue, SQUARE_MINUTE);
@@ -300,10 +300,10 @@ public class SquareTimeTest {
 		int squareDayValue = 1;
 		int squareWeekValue = 1;
 		try {
-			Time squareSecondTestResult = new Time("0.00000000000273386103", SQUARE_SECOND);
-			Time squareMinuteTestResult = new Time("0.00000000984189972285", SQUARE_MINUTE);
-			Time squareHourTestResult = new Time("0.00003543083900226757", SQUARE_HOUR);
-			Time squareDayTestResult = new Time("0.02040816326530612245", SQUARE_DAY);
+			Time squareSecondTestResult = new Time("0.00000000000273386103", SQUARE_WEEK);
+			Time squareMinuteTestResult = new Time("0.00000000984189972285", SQUARE_WEEK);
+			Time squareHourTestResult = new Time("0.00003543083900226757", SQUARE_WEEK);
+			Time squareDayTestResult = new Time("0.02040816326530612245", SQUARE_WEEK);
 			Time squareWeekTestResult = new Time("1.00000000000000000000", SQUARE_WEEK);
 
 			Time squareSecondResult = toSquareWeek(squareSecondValue, SQUARE_SECOND);
