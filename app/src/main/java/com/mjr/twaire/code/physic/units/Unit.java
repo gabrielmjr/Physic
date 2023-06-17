1d56f57ccb805977699dd8cc709b663af2193c76
GabrielMJr <gmjplayys@gmail.com>
2023-06-17 08:01:25 +0200

    feat: created abstract unit class

---
+++ app/src/main/java/com/mjr/twaire/code/physic/units/Unit.java
@@ -0,0 +1,46 @@
+package com.mjr.twaire.code.physic.units;
+
+import java.math.BigDecimal;
+
+public abstract class Unit {
+    protected BigDecimal value;
+    protected int unit;
+    
+    public Unit() {
+        value = BigDecimal.ZERO;
+    }
+
+    public Unit(double value) {
+        this.value = BigDecimal.valueOf(value);
+    }
+
+    public Unit(String value) {
+        this.value = new BigDecimal(value);
+    }
+
+    public Unit(long value) {
+        this.value = BigDecimal.valueOf(value);
+    }
+
+    public Unit(BigDecimal value) {
+        this.value = value;
+    }
+
+    public Unit setValue(BigDecimal value) {
+        this.value = value;
+        return this;
+    }
+
+    public BigDecimal getValue() {
+        return value;
+    }
+
+    public Unit setUnit(int unit) {
+        this.unit = unit;
+        return this;
+    }
+
+    public int getUnit() {
+        return unit;
+    }
+}
